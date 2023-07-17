f86fa11dc797d445d9f2e9e9a240dd84ddd1258b
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-17 12:23:58 +0530

    Added:Using Hashtable to find frequency of the sentence

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class HashTable {
     public static void main(String[] args) {
-        System.out.println("Welcome to Generics Hashtable Program");
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        String input = "To be or not to be";
+        String[] inputArray = input.split(" ");
+        for (String str : inputArray) {
+            if (hashtable.containsKey(str)) {
+                hashtable.put(str, hashtable.get(str) + 1);
+            } else {
+                hashtable.put(str, 1);
+            }
+        }
+        for (Map.Entry entry : hashtable.entrySet()) {
+            System.out.println(entry.getKey() + "  " + entry.getValue());
+        }
     }
 }
