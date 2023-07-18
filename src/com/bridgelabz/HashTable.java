0815c200ac972089cd191fd61722758164bbd54b
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-18 12:30:11 +0530

    Added :Using regex removing the avoidable word in the sentence

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -6,7 +6,8 @@ import java.util.Map;
 public class HashTable {
     public static void main(String[] args) {
         Hashtable<String, Integer> hashtable = new Hashtable<>();
-        String input =" Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        input = input.replace("avoidable", "");
         String[] inputArray = input.split(" ");
         for (String str : inputArray) {
             if (hashtable.containsKey(str)) {
@@ -18,5 +19,6 @@ public class HashTable {
         for (Map.Entry entry : hashtable.entrySet()) {
             System.out.println(entry.getKey() + " --> " + entry.getValue());
         }
+
     }
 }
